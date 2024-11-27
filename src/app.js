849dd10bcec40efb796844802a31b1a828e5f79d
Shruti Sharma <shruti.sharma@leverageedu.com>
2024-11-27 13:19:39 +0530

    Middlewares and Error Handling

--- src/app.js
+++ src/app.js
@@ -1,15 +1,35 @@
+const {adminAuth,userAuth} = require("./middlewares/auth");
+
 const express = require("express");
 
 const app = express();
 
-app.use("/hello",(req,res) => {
-    res.send("Hello Hello World");
+//Handling auth middleware for all GET, POST ... requests
+app.use("/admin",adminAuth)
+app.get("/admin/getAllData",(req,res) => {
+    //always use try catch method to handle errors
+    try{
+        throw new Error("sgjfehfoirehfoerh");
+        res.send("Hello admin !!");
+    }catch(err) {       
+        res.status(500).send("Something went wrong");
+    }
+    console.log("Hello user !!");
+    res.send("Hello user !!");
 })
 
-app.use("/", (req,res) => {
-    res.send("Hello World !!!");
+app.get("/user/getAllData",userAuth,(req,res) => {
+    console.log("Hello user !!");                   //we can write in both the ways, either call the middleware through app.use or directly call it in the route.
+    res.send("Hello user !!");
 })
 
+app.use("/",(err,req,res,next) => {
+    if(err) {
+        res.status(500).send("Something went wrong");
+    }else {
+
+    }
+})
 app.listen(3000, () => {
     console.log("Server successfully listening on port 3000");
 })
