5c4a875386bda2267516e0111a9682d45c5072bf
Shruti Sharma <shruti.sharma@leverageedu.com>
2024-11-29 13:56:55 +0530

    adding dynamic data in signup api

--- src/app.js
+++ src/app.js
@@ -1,9 +1,29 @@
 const {adminAuth,userAuth} = require("./middlewares/auth");
-require("./config/database");
+const connectDB = require("./config/database");
+const User = require("./models/user");
 const express = require("express");
 
 const app = express();
 
-app.listen(3000, () => {
-    console.log("Server successfully listening on port 3000");
+app.use(express.json());
+
+app.post("/signup", async (req, res) => {
+    const user = new User(req.body);
+    try{
+        await user.save();
+        res.send("User signed up successfully");
+    }catch{
+        res.status(500).send("Something went wrong");
+    }
+    
+    
 })
+
+connectDB().then(() => {
+    console.log("Connected to MongoDB");
+    app.listen(7000, () => {
+        console.log("Server successfully listening on port 7000");
+    })
+}).catch((error) => {
+    console.error("Error connecting to MongoDB:", error);
+});
--- src/config/database.js
+++ src/config/database.js
@@ -2,11 +2,7 @@ const mongoose = require("mongoose");
 
 
 const connectDB = async () =>{
-    await mongoose.connect("mongodb+srv://Shruti2303:Mummy1976@mernapp.qnmhsay.mongodb.net/");
+    await mongoose.connect("mongodb+srv://Shruti2303:Mummy1976@mernapp.qnmhsay.mongodb.net/devTinder");
 };
 
-connectDB().then(() => {
-    console.log("Connected to MongoDB");
-}).catch((error) => {
-    console.error("Error connecting to MongoDB:", error);
-});
+module.exports = connectDB;
