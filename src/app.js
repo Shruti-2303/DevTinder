b999292cf4892c14968715c82a0a68d56b458366
Shruti Sharma <shruti.sharma@leverageedu.com>
2024-11-29 17:39:30 +0530

    delete api added

--- src/app.js
+++ src/app.js
@@ -6,7 +6,15 @@ const express = require("express");
 const app = express();
 
 app.use(express.json());
-
+app.delete("/delete", async(req,res) => {
+    const userId = req.body.userId;
+    try{
+        const user = await User.findByIdAndDelete(userId);
+        res.send("User deleted successfully");
+    }catch(err){
+        res.status(400).send("Something went wrong" + err.message);
+    }
+})
 app.get("/users", async(req,res) => {
     const userEmail = req.body.emailId;
 
