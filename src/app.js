fa19b1e54ab2a3f6f11489295da46d236bf09081
Shruti Sharma <shruti.sharma@leverageedu.com>
2024-12-12 16:13:15 +0530

    Hashing and login api done

--- src/app.js
+++ src/app.js
@@ -2,6 +2,8 @@ const {adminAuth,userAuth} = require("./middlewares/auth");
 const connectDB = require("./config/database");
 const User = require("./models/user");
 const express = require("express");
+const { validateSignUp } = require("./utils/validation");
+const bcrypt = require("bcrypt");
 
 const app = express();
 
@@ -35,13 +37,45 @@ app.get("/feed", async(req,res) => {
         res.status(400).send("Something went wrong" + err.message);
     }
 })
-app.post("/signup", async (req, res) => {
-    const user = new User(req.body);
+app.post('/login', async(req,res) => {
     try{
-        await user.save();
-        res.send("User signed up successfully");
+        const {emailId, password} = req.body;
+        const user = await User.findOne({emailId : emailId});
+        if(!user){
+            throw new Error("Invalid credentials");
+        }
+        
+        const isPasswordValid = await bcrypt.compare(password,user.password);
+        if(!isPasswordValid){
+            throw new Error("Invalid credentials");
+        }
+        else{
+            res.send("User logged in successfully");
+        }
     }catch(err){
-        res.status(500).send("Something went wrong" + err.message);
+        res.status(400).send("ERROR : " + err.message);
+    }
+})
+app.post("/signup", async (req, res) => {
+
+    try{
+        const {firstName, lastName, emailId, password} = req.body;
+        //validate the user data
+        validateSignUp(req);
+        //Encrypt the password
+        const passwordHash = await bcrypt.hash(password,10);
+        //Save the user
+        const user = new User({
+            firstName,
+            lastName,    
+            emailId,
+            password : passwordHash,
+        });
+        
+            await user.save();
+            res.send("User signed up successfully");
+    }catch(err){
+        res.status(500).send("ERROR : " + err.message);
     }
     
     
